2b25e456033c2f8cc7969a3d51dda036d3f5c472
Antiquitas2004 <46765729+Antiquitas2004@users.noreply.github.com>
2019-01-30 23:09:17 -0500

    Update bikeclass.java

--- bikeclass.java
+++ bikeclass.java
@@ -1,33 +1,33 @@
-class Bicycle 
+public class Bicycle 
 {
-{
-int cadence = 0; //variables
-int speed = 0;
-int gear = 1;
-
-void changeCadence(int Value)
-{//methods
-cadence=Value;
-}
-
-void changeGear (int newValue)
-{
-gear=newValue;
-}
-
-void speedUp(int increment)
-{
-speed=speed+increment;
-}
-
-void applyBrakes(int decrement)
-{
-speed=speed-decrement;
-}
-
-void printStates() 
-{
-	//print out our current states
-System.out.println("cadence:"+ cadence + "speed:"+ speed+ "gear:"+gear);
-}
+    public int cadence;
+    public int gear;
+    public int speed;
+        
+    public Bicycle(int startCadence, int startSpeed, int startGear) 
+    {
+        gear = startGear;
+        cadence = startCadence;
+        speed = startSpeed;
+    }
+    public void setCadence(int newValue) 
+    {
+        cadence = newValue;
+    }
+        
+    public void setGear(int newValue) 
+    {
+        gear = newValue;
+    }
+        
+    public void applyBrake(int decrement) 
+    {
+        speed -= decrement;
+    }
+        
+    public void speedUp(int increment) 
+    {
+        speed += increment;
+    }
+        
 }
